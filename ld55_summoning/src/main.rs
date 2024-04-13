364e36b914292b1d82ecb1c38e314c8a02a3621c
Joel Davis <joeld@goodtrouble.team>
2024-04-12 21:28:48 -0700

    spawn cards

--- ld55_summoning/src/main.rs
+++ ld55_summoning/src/main.rs
@@ -2,6 +2,20 @@
 
 use bevy::prelude::*;
 
+// use bevy_prng::WyRand;
+// use bevy_rand::prelude::GlobalEntropy;
+// use rand_core::RngCore;
+
+use rand::Rng;
+
+#[derive(Resource,Default)]
+struct CardDeck {
+    texture: Handle<Image>,
+    layout: Handle<TextureAtlasLayout>,
+
+    // todo: card stats, etc 
+}
+
 fn main() {
 
     App::new()    
@@ -13,7 +27,9 @@ fn main() {
             }),
             ..default()                         
         }))
+        .insert_resource( CardDeck::default() )
         .add_systems(Startup, setup)
+        .add_systems( Update, spawn_cards)
         .run();
 }
 
@@ -23,6 +39,7 @@ fn setup(
     mut meshes: ResMut<Assets<Mesh>>,    
     mut materials: ResMut<Assets<StandardMaterial>>,
     mut texture_atlas_layouts: ResMut<Assets<TextureAtlasLayout>>,
+    mut cards: ResMut<CardDeck>,
     asset_server: Res<AssetServer>
 ) {
     // circular base
@@ -55,19 +72,7 @@ fn setup(
         ..default()
     });
 
-
-
     // 2D scene -------------------------------
-    // Load card atlas
-    let texture = asset_server.load("cardfish_cards.png");
-    let layout = TextureAtlasLayout::from_grid(
-        Vec2::new(180.0, 256.0), 11, 2, None, None);
-    let texture_atlas_layout = texture_atlas_layouts.add(layout);
-
-
-    
-
-
     commands.spawn(Camera2dBundle { 
         camera: Camera {
             order: 2, // Draw sprites on top of 3d world
@@ -76,19 +81,53 @@ fn setup(
         ..default()
     });
 
-    commands.spawn((
-        SpriteSheetBundle {
-            texture,
-            atlas: TextureAtlas {
-                layout: texture_atlas_layout,
-                index: 0,
-            },            
-            ..default()
-        },        
-    ));
+    // Load card atlas
+    let texture = asset_server.load("cardfish_cards.png");
+    let layout = TextureAtlasLayout::from_grid(
+        Vec2::new( 567.0*(256.0/811.0), 256.0), 11, 2, None, None);
+    let texture_atlas_layout = texture_atlas_layouts.add(layout);
+
+    cards.texture = texture;
+    cards.layout = texture_atlas_layout;
+
+    // commands.spawn((
+    //     SpriteSheetBundle {
+    //         texture,
+    //         atlas: TextureAtlas {
+    //             layout: texture_atlas_layout,
+    //             index: 0,
+    //         },            
+    //         ..default()
+    //     },        
+    // ));
 
     commands.spawn(SpriteBundle {
         texture: asset_server.load("bevy_bird_dark.png"),
         ..default()
     });
 }
+
+fn spawn_cards ( 
+    mut commands: Commands,        
+    cards: Res<CardDeck>,
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+)
+{
+    if keyboard_input.just_pressed( KeyCode::KeyW ) {
+        println!("W pressed");
+        let mut rng = rand::thread_rng();
+        commands.spawn((
+            SpriteSheetBundle {
+                texture: cards.texture.clone(),
+                atlas: TextureAtlas {
+                    layout: cards.layout.clone(),
+                    index: rng.gen_range(1..20),
+                },     
+                //Transform::from_xyz(rng.gen<f32>() * 100.0, 0.0f, rng.gen<f32>() * 100.0),       
+                transform: Transform::from_xyz(rng.gen::<f32>() * 1000.0 - 500.0, rng.gen::<f32>() * 700.0 - 350.0, 0.0),
+                ..default()
+            },        
+        ));
+    }
+}
+
