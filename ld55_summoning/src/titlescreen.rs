310e95a84fad13c19455f7df3f8c06881eee84c8
Joel Davis <joeld@goodtrouble.team>
2024-04-26 17:48:04 -0700

    Player select tiles WIP

--- ld55_summoning/src/titlescreen.rs
+++ ld55_summoning/src/titlescreen.rs
@@ -1,4 +1,4 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, scene::ron::de};
 use crate::summongame::{ GameAppState, PlayerType, GoodStuff };
 
 #[derive(Component)]
@@ -22,7 +22,8 @@ impl Plugin for TitleScreenPlugin {
             .add_systems( OnEnter(GameAppState::TitleScreen), title_setup )
             .add_systems(Update, (
                 title_update,
-                player_settings)
+                //player_settings
+                )
                 .run_if(in_state(GameAppState::TitleScreen)))
             .add_systems( OnExit(GameAppState::TitleScreen), title_teardown )
             .add_event::<PlayerSettingsChanged>();
@@ -37,46 +38,247 @@ fn title_setup(
 ) {
     println!("Title screen setup!");
 
-    commands.spawn((SpriteBundle {
-        texture: asset_server.load("title.png"),
-        transform: Transform::from_xyz( 0.0, 0.0, 3.0 ).with_scale( Vec3::splat( 0.6)),
-        ..default()
-    }, TitleScreenCleanup ));
+
+    // commands.spawn((SpriteBundle {
+    //     texture: asset_server.load("title.png"),
+    //     transform: Transform::from_xyz( 0.0, 0.0, 3.0 ).with_scale( Vec3::splat( 0.6)),
+    //     ..default()
+    // }, TitleScreenCleanup ));
 
 
-    commands.spawn((
-        TextBundle::from_section(
-            "Title Screen Goes Here",
-            TextStyle {
-                font_size: 20.,
+    // commands.spawn((
+    //     TextBundle::from_section(
+    //         "Title Screen Goes Here",
+    //         TextStyle {
+    //             font_size: 20.,
+    //             ..default()
+    //         },
+    //     )
+    //     .with_style(Style {
+    //         position_type: PositionType::Absolute,
+    //         top: Val::Px(12.0),
+    //         left: Val::Px(12.0),
+    //         ..default()
+    //     }),
+    //     TitleScreenCleanup,
+    // ));
+
+    let title_img = asset_server.load("summoner_title.png");
+    let playerframe_img = asset_server.load("ui_playerframe.png");
+    let border_img = asset_server.load("panel-transparent-border-027.png");
+
+    let portrait_imgs = asset_server.load("portrait1.png");
+
+
+    let slicer = TextureSlicer {
+        border: BorderRect::square(22.0),
+        center_scale_mode: SliceScaleMode::Stretch,
+        sides_scale_mode: SliceScaleMode::Stretch,
+        max_corner_scale: 1.0,
+    };
+
+    let title_sz = 80.0;
+    commands
+        .spawn(NodeBundle {
+            style: Style {
+                left: Val::Percent((100.0 - title_sz) / 2.0),
+                width: Val::Percent(title_sz),
+                height: Val::Percent(100.0),
+                align_items: AlignItems::Center,
+                justify_content: JustifyContent::SpaceBetween,
+                flex_direction: FlexDirection::Column,
                 ..default()
             },
-        )
-        .with_style(Style {
-            position_type: PositionType::Absolute,
-            top: Val::Px(12.0),
-            left: Val::Px(12.0),
-            ..default()
-        }),
-        TitleScreenCleanup,
-    ));
-
-    commands.spawn(
-        TextBundle::from_section(
-            "Dont Delete Me",
-            TextStyle {
-                color: Color::GREEN,
-                font_size: 20.,
-                ..default()
-            },
-        )
-        .with_style(Style {
-            position_type: PositionType::Absolute,
-            top: Val::Px(30.0),
-            left: Val::Px(12.0),
+            //background_color: BackgroundColor( Color::Rgba { red: 1.0, green: 0.0, blue: 0.0, alpha: 0.2 } ),
             ..default()
         })
-    );
+        .with_children(|parent| {
+
+
+            // ---- Cyber Summoner Title Image -----------------------
+            parent.spawn(ImageBundle {
+                style: Style {
+                    width: Val::Percent(100.0),
+                    margin: UiRect::top( Val::Percent( 5.0 )),
+                    ..default()
+                },
+                image: UiImage::new(title_img),
+                //background_color: BackgroundColor( Color::Rgba { red: 1.0, green: 1.0, blue: 1.0, alpha: 0.01 } ),
+                ..default()
+            });
+
+            // ---- Player Select Tiles -----------------------
+            let tile_scale = 0.6;
+            parent.spawn( NodeBundle {
+                style: Style {
+                    flex_wrap: FlexWrap::Wrap,
+                    //align_items: AlignItems::SpaceEvenly,
+                    justify_content: JustifyContent::SpaceEvenly,
+                    ..default()
+                },
+                //background_color: BackgroundColor( Color::Rgba { red: 0.0, green: 0.0, blue: 1.0, alpha: 0.2 } ),
+                ..default()
+            }).with_children( |tileparent| {
+
+                for i in 0..4 {
+
+                    tileparent.spawn(
+
+                        ImageBundle {
+                            style: Style {
+                                width: Val::Px(156.0 * tile_scale),
+                                height: Val::Px(234.0 * tile_scale),
+                                margin: UiRect::all( Val::Px( 8.0 )),
+                                flex_direction: FlexDirection::Column,
+                                align_items: AlignItems::Center,
+                                ..default()
+                            },
+                            image: UiImage::new(playerframe_img.clone() ),
+                            background_color: BackgroundColor( stuff.player_stuff[i].color, ),
+                            ..default()
+                        })
+                        .with_children(|parent| {
+
+                            //==== Player#1 header
+                            parent.spawn(TextBundle::from_section(
+                                format!("PLR0{}", (i+1) ).clone(),
+                                TextStyle {
+                                    //font: asset_server.load("Cyberthrone.ttf"),
+                                    font_size: 12.0,
+                                    color: Color::rgb(1.0, 1.0, 1.0),
+                                    ..default()
+                                },
+                            ).with_style( Style {
+                                width: Val::Px(130.0 * tile_scale),
+                                margin: UiRect::new( Val::Px( 12.0 ), Val::Px( 8.0 ), Val::Px( 4.0 ), Val::Px( 6.0 )),
+                                ..default()
+                            }));
+
+                            // Portrait
+                            parent.spawn( ImageBundle {
+                                style: Style {
+                                    width: Val::Px( 100.0 * tile_scale ),
+                                    height: Val::Px( 100.0 * tile_scale  ),
+                                    ..default()
+                                },
+                                image: UiImage::new( portrait_imgs.clone() ),
+                                ..default()
+                            });
+
+                            //==== PlayerName
+                            parent.spawn(TextBundle::from_section(
+                                "Human",
+                                TextStyle {
+                                    //font: asset_server.load("Cyberthrone.ttf"),
+                                    font_size: 20.0,
+                                    color: stuff.player_stuff[i].color,
+                                    ..default()
+                                },
+                            ).with_style( Style {
+                                width: Val::Px(130.0 * tile_scale),
+                                margin: UiRect::new( Val::Px( 40.0 ), Val::Px( 20.0 ),
+                                                        Val::Px( 6.0 ), Val::Px( 4.0 )),
+                                ..default()
+                            }));
+
+
+
+
+                            // Human/AI/None Selection Bar
+                            parent.spawn( NodeBundle {
+                                style: Style {
+                                    width: Val::Percent(100.0),
+                                    //flex_wrap: FlexWrap::Wrap,
+                                    //align_items: AlignItems::SpaceEvenly,
+                                    justify_content: JustifyContent::SpaceEvenly,
+                                    ..default()
+                                },
+                                background_color: BackgroundColor( stuff.player_stuff[i].color ),
+                                ..default()
+                            })
+                            .with_children( |btnparent| {
+
+                                //for btn_ndx in 0..3 {
+                                let btn_names = ["Human", "AI", "None" ];
+                                for btn_ndx in 0..btn_names.len() {
+                                    let btn_name = btn_names[ btn_ndx];
+
+                                    let (btncolor, txtcolor) = if btn_name == "Human" {
+                                        (Color::rgba(0.0, 0.0, 0.0, 0.5),
+                                        Color::WHITE )
+                                    } else {
+                                        (Color::rgba(0.0, 0.0, 0.0, 0.0),
+                                            stuff.player_stuff[i].color2
+                                        )
+                                    };
+
+                                    btnparent.spawn(
+                                        ButtonBundle {
+                                            style: Style {
+                                                justify_content: JustifyContent::Center,
+                                                align_items: AlignItems::Center,
+                                                ..default()
+                                            },
+                                            background_color: BackgroundColor( btncolor  ),
+                                            ..default()
+                                        }
+                                    )
+                                    .with_children(|parent| {
+                                        parent.spawn(TextBundle::from_section(
+                                            btn_name,
+                                            TextStyle {
+                                                //font: asset_server.load("Cyberthrone.ttf"),
+                                                font_size: 12.0,
+                                                color: txtcolor,
+                                                ..default()
+                                            },
+                                        ));
+                                    });
+                                }
+
+                            });
+
+                        });
+                }
+
+            });
+
+
+            // ---- Start Game Button -----------------------
+            parent
+                .spawn((
+                    ButtonBundle {
+                        style: Style {
+                            width: Val::Percent(50.0),
+                            height: Val::Px(60.0),
+                            justify_content: JustifyContent::Center,
+                            // vertically center child text
+                            align_items: AlignItems::Center,
+                            //margin: UiRect::all(Val::Px(20.0)),
+                            margin: UiRect::new( Val::Px(20.0), Val::Px(20.0),Val::Px(20.0),Val::Px(50.0) ),
+                            ..default()
+                        },
+                        image: border_img.clone().into(),
+                        ..default()
+                    },
+                    ImageScaleMode::Sliced(slicer.clone()),
+                ))
+                .with_children(|parent| {
+                    parent.spawn(TextBundle::from_section(
+                        "Start Game",
+                        TextStyle {
+                            font: asset_server.load("Cyberthrone.ttf"),
+                            font_size: 50.0,
+                            color: Color::rgb(1.0, 0.3, 0.9),
+                        },
+                    ));
+                });
+
+        });
+
+
+
+
 
     // setup player status
     stuff.player_stuff[0].ptype = PlayerType::Local;
@@ -84,27 +286,29 @@ fn title_setup(
     stuff.player_stuff[2].ptype = PlayerType::AI;
     stuff.player_stuff[3].ptype = PlayerType::NotActive;
 
-    let mut yy = 350.0;
-    for i in 0..4 {
 
-            commands.spawn((
-                TextBundle::from_section("Player # -- ???",
-                    TextStyle {
-                        color: stuff.player_stuff[i].color,
-                        font_size: 30.,
-                        ..default()
-                    },
-                )
-                .with_style(Style {
-                    position_type: PositionType::Absolute,
-                    top: Val::Px(yy),
-                    left: Val::Px( 300.0),
-                    ..default()
-                }),
-                PlayerSetting(i as i32),
-                TitleScreenCleanup) );
-            yy += 30.0;
-    }
+    // let mut yy = 350.0;
+    // for i in 0..4 {
+
+    //         commands.spawn((
+    //             TextBundle::from_section("Player # -- ???",
+    //                 TextStyle {
+    //                     color: stuff.player_stuff[i].color,
+    //                     font_size: 30.,
+    //                     ..default()
+    //                 },
+    //             )
+    //             .with_style(Style {
+    //                 position_type: PositionType::Absolute,
+    //                 top: Val::Px(yy),
+    //                 left: Val::Px( 300.0),
+    //                 ..default()
+    //             }),
+
+    //             PlayerSetting(i as i32),
+    //             TitleScreenCleanup) );
+    //         yy += 30.0;
+    // }
 
     ev_settings.send( PlayerSettingsChanged );
 
@@ -119,7 +323,7 @@ fn title_update (
     mut ev_settings: EventWriter<PlayerSettingsChanged>,
 )
 {
-    println!("Titles update...");
+ //   println!("Titles update...");
 
     let mut z : i32 = -1;
     for i in 0..4 {
